5e88e3c7c2455a69faf8ba349271ddb168a4645d
acstylos <a.stylos983@gmail.com>
2019-04-29 16:26:24 -0400

    Minor Changes to Panda_robot_control

--- src/panda_robot_control/src/robot_mover.cpp
+++ src/panda_robot_control/src/robot_mover.cpp
@@ -12,13 +12,13 @@ int main(int argc, char** argv)
     //ros::Subscriber imagesSubscriber = nodeHandler.subscribe("/panda_camera/images", 1000, callback);
 
     // Publish to each joint individually
-    ros::Publisher joint1Pub = nodeHandler.advertise<int>("/joint1_position_controller/command", 1000);
-    ros::Publisher joint2Pub = nodeHandler.advertise<int>("/joint2_position_controller/command", 1000);
-    ros::Publisher joint3Pub = nodeHandler.advertise<int>("/joint3_position_controller/command", 1000);
-    ros::Publisher joint4Pub = nodeHandler.advertise<int>("/joint4_position_controller/command", 1000);
-    ros::Publisher joint5Pub = nodeHandler.advertise<int>("/joint5_position_controller/command", 1000);
-    ros::Publisher joint6Pub = nodeHandler.advertise<int>("/joint6_position_controller/command", 1000);
-    ros::Publisher joint7Pub = nodeHandler.advertise<int>("/joint7_position_controller/command", 1000);
+    ros::Publisher joint1Pub = nodeHandler.advertise<int32_t>("/joint1_position_controller/command", 1000);
+    ros::Publisher joint2Pub = nodeHandler.advertise<int32_t>("/joint2_position_controller/command", 1000);
+    ros::Publisher joint3Pub = nodeHandler.advertise<int32_t>("/joint3_position_controller/command", 1000);
+    ros::Publisher joint4Pub = nodeHandler.advertise<int32_t>("/joint4_position_controller/command", 1000);
+    ros::Publisher joint5Pub = nodeHandler.advertise<int32_t>("/joint5_position_controller/command", 1000);
+    ros::Publisher joint6Pub = nodeHandler.advertise<int32_t>("/joint6_position_controller/command", 1000);
+    ros::Publisher joint7Pub = nodeHandler.advertise<int32_t>("/joint7_position_controller/command", 1000);
 
 
     ros::Rate loop_rate(10);
