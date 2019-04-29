32b00ba2d8cfa61917ebd75ed8f7942eabf20fab
acstylos <a.stylos983@gmail.com>
2019-04-29 18:46:34 -0400

    Final attempt at ros nodes (failure)

--- src/panda_robot_control/src/robot_mover.cpp
+++ src/panda_robot_control/src/robot_mover.cpp
@@ -1,6 +1,6 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
-#include "std_msgs/Int32.h"
+#include "std_msgs/Float64.h"
 
 
 int main(int argc, char** argv)
@@ -12,13 +12,13 @@ int main(int argc, char** argv)
     //ros::Subscriber imagesSubscriber = nodeHandler.subscribe("/panda_camera/images", 1000, callback);
 
     // Publish to each joint individually
-    //ros::Publisher joint1Pub = nodeHandler.advertise<std_msgs::Int32>("/joint1_position_controller/command", 1000);
-    //ros::Publisher joint2Pub = nodeHandler.advertise<std_msg::Int32>("/joint2_position_controller/command", 1000);
-    //ros::Publisher joint3Pub = nodeHandler.advertise<std_msg::Int32>("/joint3_position_controller/command", 1000);
-    //ros::Publisher joint4Pub = nodeHandler.advertise<std_msg::Int32>("/joint4_position_controller/command", 1000);
-    //ros::Publisher joint5Pub = nodeHandler.advertise<std_msg::Int32>("/joint5_position_controller/command", 1000);
-    //ros::Publisher joint6Pub = nodeHandler.advertise<std_msg::Int32>("/joint6_position_controller/command", 1000);
-    //ros::Publisher joint7Pub = nodeHandler.advertise<std_msg::Int32>("/joint7_position_controller/command", 1000);
+    ros::Publisher joint1Pub = nodeHandler.advertise<std_msgs::Float64>("/joint1_position_controller/command", 1000);
+    ros::Publisher joint2Pub = nodeHandler.advertise<std_msgs::Float64>("/joint2_position_controller/command", 1000);
+    ros::Publisher joint3Pub = nodeHandler.advertise<std_msgs::Float64>("/joint3_position_controller/command", 1000);
+    ros::Publisher joint4Pub = nodeHandler.advertise<std_msgs::Float64>("/joint4_position_controller/command", 1000);
+    ros::Publisher joint5Pub = nodeHandler.advertise<std_msgs::Float64>("/joint5_position_controller/command", 1000);
+    ros::Publisher joint6Pub = nodeHandler.advertise<std_msgs::Float64>("/joint6_position_controller/command", 1000);
+    ros::Publisher joint7Pub = nodeHandler.advertise<std_msgs::Float64>("/joint7_position_controller/command", 1000);
 
 
     ros::Rate loop_rate(10);
@@ -26,15 +26,15 @@ int main(int argc, char** argv)
 
     while(ros::ok())
     {
-        std_msgs::Int32 examplePose;
-        examplePose.data = 90;
-        //joint1Pub.publish(examplePose);
-        //joint2Pub.publish(examplePose);
-        //joint3Pub.publish(examplePose);
-        //joint4Pub.publish(examplePose);
-        //joint5Pub.publish(examplePose);
-        //joint6Pub.publish(examplePose);
-        //joint7Pub.publish(examplePose);
+        std_msgs::Float64 examplePose;
+        examplePose.data = 1.5;
+        joint1Pub.publish(examplePose);
+        joint2Pub.publish(examplePose);
+        joint3Pub.publish(examplePose);
+        joint4Pub.publish(examplePose);
+        joint5Pub.publish(examplePose);
+        joint6Pub.publish(examplePose);
+        joint7Pub.publish(examplePose);
 
         ros::spinOnce();
         loop_rate.sleep();
